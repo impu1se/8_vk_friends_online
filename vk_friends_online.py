f1ec559edcb008d6dfb8b74830b7f0c5501ec974
impu1se <impu1se@bk.ru>
2016-11-18 16:23:16 +0300

    Finally

--- vk_friends_online.py
+++ vk_friends_online.py
@@ -1,29 +1,38 @@
 import vk
+import sys
+from getpass import getpass
 
 
-APP_ID = -1  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
+APP_ID = 5724110  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
 
 
 def get_user_login():
-    pass
+    return input('Input your email: ')
 
 
 def get_user_password():
-    pass
+    return getpass('Input your password: ')
 
 
 def get_online_friends(login, password):
-    session = vk.AuthSession(
+    try:
+        session = vk.AuthSession(
         app_id=APP_ID,
         user_login=login,
         user_password=password,
+        scope='friends'
     )
+
+    except vk.exceptions.VkAuthError:
+        sys.exit('Authorization error (incorrect password)')
     api = vk.API(session)
-    # например, api.friends.get()
+    return api.users.get(user_ids=api.friends.getOnline())
 
 
 def output_friends_to_console(friends_online):
-    pass
+    for friend in friends_online:
+        print(friend['first_name'], friend['last_name'])
+
 
 if __name__ == '__main__':
     login = get_user_login()
